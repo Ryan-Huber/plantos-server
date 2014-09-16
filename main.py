d0a0aae9740952de7f060be84062959566917776
Leon Chambers <leon8556@mit.edu>
2014-09-16 19:11:44 -0400

    Started implementing debug mode

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sensors.constants import *
 # General imports
 import time
 import json
+import argparse
 import gevent.monkey
 gevent.monkey.patch_thread()
 from threading import Thread
@@ -12,24 +13,11 @@ from threading import Thread
 from flask import Flask
 from flask import render_template
 flask_app = Flask(__name__, instance_path="/var/lib/plantos-server")
-flask_app.config['DEBUG'] = True
 # SocketIO Setup
 from flask.ext.socketio import SocketIO
 socketio = SocketIO(flask_app)
 # Pymongo Setup
-with flask_app.open_instance_resource("database_credentials.txt") as f:
-    (username, password) = f.readline()[:-1].split(",")
-    flask_app.config["MONGO_USERNAME"] = username
-    flask_app.config["MONGO_PASSWORD"] = password
 from pymongo import MongoClient
-client = MongoClient()
-client.admin.authenticate(username,password)
-
-# Register Blueprints
-from plants.main import plants
-flask_app.register_blueprint(plants)
-from sensors.main import sensors
-flask_app.register_blueprint(sensors)
 
 @flask_app.route("/")
 def main_page():
@@ -56,6 +44,29 @@ def background_thread(system, board):
         time.sleep(1)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the CityFARM web server")
+    parser.add_argument("-d", "--debug", action="store_true", help="enable flask\
+            debug mode")
+    args = parser.parse_args()
+    flask_app.config["DEBUG"] = args.debug
+    # PyMongo Setup
+    if args.debug:
+        # We might not be running on the server, so explicitly supply ip address
+        client = MongoClient("18.85.58.66")
+    else:
+        # We are running on the server, connect via localhost
+        client = MongoClient()
+    # Authenticate to the server
+    with flask_app.open_instance_resource("database_credentials.txt") as f:
+        (username, password) = f.readline()[:-1].split(",")
+        flask_app.config["MONGO_USERNAME"] = username
+        flask_app.config["MONGO_PASSWORD"] = password
+    client.admin.authenticate(username,password)
+    # Register Blueprints
+    from plants.main import plants
+    flask_app.register_blueprint(plants)
+    from sensors.main import sensors
+    flask_app.register_blueprint(sensors)
     for system in DATABASE_NAMES.keys():
         for collection in COLLECTION_NAMES[system].keys():
             # Start a thread to monitor the collection
